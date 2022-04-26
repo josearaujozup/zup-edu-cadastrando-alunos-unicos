639ab497c7928018d76b8661225bf0919fd588c0
Denes <jose.araujo@zup.com.br>
2022-04-26 20:57:59 -0300

    commit inicial

---
+++ src/main/java/br/com/zup/edu/alunounico/AlunounicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.alunounico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlunounicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlunounicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/edu/alunounico/AlunounicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.alunounico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunounicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
